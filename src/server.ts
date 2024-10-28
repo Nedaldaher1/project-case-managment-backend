3d628f422678b95d9d6c1c2754a6f18038f572fa
NedalDaher128 <progammerabood@gmail.com>
2024-10-29 00:28:39 +0300

    new update

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,14 @@
 import { serve } from '@hono/node-server'; // Import the Hono serve function
 import app from './app.js';
 
+
+const PORT = parseInt(process.env.PORT || '4000', 10); // You can change the default port here
+
+serve({
+  fetch: app.fetch,
+  port: PORT,
+})
+
 serve(app, (info) => {
   console.log(`Server is running on ${info.address}`);
 });
